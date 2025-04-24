88e3313b164ec2f9a9cbe194cafcdef00244bc7c
Sparsh Bansal <sparshbansal787@gmail.com>
2025-04-24 21:38:20 +0530

    frontend updated

--- Frontend/src/components/GeneralScreens/Footer.js
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -4,12 +4,14 @@ import '../../Css/Footer.css'
 const Footer = () => {
     return (
         <div>
+
+            
             <div className="footer">
             </div>
             <div className="copyright">
                 <p className="copyright-blog">© 2025 Sparsh Bansal. All rights reserved.</p>
 
-                
+
                 
             </div>
         </div>
